e9b3b83c6d820ac1c1565f6106bca1c839339fe1
salao <salahdin.belghiti@movicoders.com>
2020-09-22 11:59:27 +0200

    commit

--- src/main/java/com/movilizer/microservices/commons/models/Log.java
+++ src/main/java/com/movilizer/microservices/commons/models/Log.java
@@ -1,17 +1,15 @@
 package com.movilizer.microservices.commons.models;
 
-import java.util.Calendar;
-
 public class Log {
 
     int id;
-    Calendar calendar;
+    String calendar;
     String component;
     int severity;
     String message;
     String username;
 
-    public Log(Calendar calendar, String component, int severity, String message, String username) {
+    public Log(String calendar, String component, int severity, String message, String username) {
         this.calendar = calendar;
         this.component = component;
         this.severity = severity;
@@ -19,8 +17,55 @@ public class Log {
         this.username = username;
     }
 
-    public static Log constructLog(Calendar calendar, String component, int severity, String message, String username) {
+    public static Log constructLog(String calendar, String component, int severity, String message, String username) {
         return new Log(calendar, component, severity, message, username);
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(String calendar) {
+        this.calendar = calendar;
+    }
+
+    public String getComponent() {
+        return component;
+    }
+
+    public void setComponent(String component) {
+        this.component = component;
+    }
+
+    public int getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(int severity) {
+        this.severity = severity;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }

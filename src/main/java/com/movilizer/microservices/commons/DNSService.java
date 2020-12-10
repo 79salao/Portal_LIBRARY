071de865ddcd1cba828eb72a1aab79d227ddc06b
salao <salahdin.belghiti@movicoders.com>
2020-12-10 09:40:10 +0100

    commit

--- src/main/java/com/movilizer/microservices/commons/DNSService.java
+++ src/main/java/com/movilizer/microservices/commons/DNSService.java
@@ -14,16 +14,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class DNSService {
 
-    private final String AWS_ACCESS_KEY_ID;
+    private String AWS_ACCESS_KEY_ID;
 
-    private final String AWS_SECRET_KEY_ID;
+    private String AWS_SECRET_KEY_ID;
 
     private final String ROUT53_HOSTED_ZONE_ID;
+
+    private final String DNS;
 
     public DNSService() {
         PropertiesConfiguration config = new PropertiesConfiguration();
@@ -32,15 +32,13 @@ public class DNSService {
         } catch (ConfigurationException e) {
             e.printStackTrace();
         }
-        this.AWS_ACCESS_KEY_ID = config.getString("aws.accesskey.id");
-
-        this.AWS_SECRET_KEY_ID = config.getString("aws.secretkey.id");
-
-        this.ROUT53_HOSTED_ZONE_ID = config.getString("aws.hostedzone.id");
+        AWS_SECRET_KEY_ID = config.getString("aws.accesskey.id");
+        AWS_SECRET_KEY_ID = config.getString("aws.secretkey.id");
+        ROUT53_HOSTED_ZONE_ID = config.getString("aws.hostedzone.id");
+        DNS = config.getString("dns");
     }
 
     public Map<String, List<String>> getMicroservicesIps() {
-        Map<String, List<String>> returnMap = new HashMap<>();
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(AWS_ACCESS_KEY_ID, AWS_SECRET_KEY_ID);
         AmazonRoute53 route53 = AmazonRoute53ClientBuilder
                 .standard()
@@ -52,25 +50,73 @@ public class DNSService {
                 .withHostedZoneId(hostedZone.getId());
         ListResourceRecordSetsResult listResourceRecordSetsResult = route53.listResourceRecordSets(listResourceRecordSetsRequest);
         List<ResourceRecordSet> resourceRecordSetList = listResourceRecordSetsResult.getResourceRecordSets();
-        for (ResourceRecordSet resourceRecordSet : resourceRecordSetList) {
-            String microserviceEndpoint = resourceRecordSet.getName();
-            String microserviceName = microserviceEndpoint.split("-")[1];
-            if (microserviceEndpoint.split("-")[0].equals("ep")) {
-                List<String> microserviceIps = new ArrayList<>();
-                for (ResourceRecord resourceRecord : resourceRecordSet.getResourceRecords()) {
-                    microserviceIps.add(resourceRecord.getValue());
-                }
-                if (returnMap.get(microserviceName) != null) {
-                    List<String> oldList = returnMap.get(microserviceName);
-                    microserviceIps = Stream.concat(oldList.stream(), microserviceIps.stream())
-                            .collect(Collectors.toList());
-                }
-                returnMap.put(microserviceName, microserviceIps);
-            }
-        }
+        Map<String, List<String>> returnMap = new HashMap<>(this.getIpsMap(resourceRecordSetList));
         System.out.println(returnMap);
         return returnMap;
     }
 
+    private Map<String, List<String>> getIpsMap(List<ResourceRecordSet> resourceRecordSetList) {
+        Map<String, List<String>> returnMap = new HashMap<>();
+        for (ResourceRecordSet resourceRecordSet : resourceRecordSetList) {
+            if (resourceRecordSet.getName().equalsIgnoreCase(DNS)) {
+                List<String> educationList = new ArrayList<>();
+                List<String> employeeList = new ArrayList<>();
+                List<String> entitiesList = new ArrayList<>();
+                List<String> experienceList = new ArrayList<>();
+                List<String> fileList = new ArrayList<>();
+                List<String> monitoringList = new ArrayList<>();
+                List<String> projectList = new ArrayList<>();
+                List<String> registerList = new ArrayList<>();
+                List<String> categoriesList = new ArrayList<>();
+                List<String> skillsList = new ArrayList<>();
+                List<String> userskillsList = new ArrayList<>();
+                List<String> dynamicpropertiesList = new ArrayList<>();
+                List<String> proxyList = new ArrayList<>();
+                List<String> authenticationList = new ArrayList<>();
+                for (ResourceRecord resourceRecord : resourceRecordSet.getResourceRecords()) {
+                    educationList.add("http://" + resourceRecord.getValue() + ":9010");
+                    employeeList.add("http://" + resourceRecord.getValue() + ":9011");
+                    entitiesList.add("http://" + resourceRecord.getValue() + ":9012");
+                    experienceList.add("http://" + resourceRecord.getValue() + ":9013");
+                    fileList.add("http://" + resourceRecord.getValue() + ":9014");
+                    monitoringList.add("http://" + resourceRecord.getValue() + ":9015");
+                    projectList.add("http://" + resourceRecord.getValue() + ":9016");
+                    registerList.add("http://" + resourceRecord.getValue() + ":9017");
+                    categoriesList.add("http://" + resourceRecord.getValue() + ":9018");
+                    skillsList.add("http://" + resourceRecord.getValue() + ":9019");
+                    userskillsList.add("http://" + resourceRecord.getValue() + ":9020");
+                    dynamicpropertiesList.add("http://" + resourceRecord.getValue() + ":9021");
+                    authenticationList.add("http://" + resourceRecord.getValue() + ":9012");
+                    proxyList.add("http://" + resourceRecord.getValue() + ":9023");
+                }
+                returnMap.put("education", educationList);
+                returnMap.put("employees", employeeList);
+                returnMap.put("entities", entitiesList);
+                returnMap.put("experiences", experienceList);
+                returnMap.put("file", fileList);
+                returnMap.put("monitoring", monitoringList);
+                returnMap.put("projects", projectList);
+                returnMap.put("register", registerList);
+                returnMap.put("categories", categoriesList);
+                returnMap.put("skills", skillsList);
+                returnMap.put("userskills", userskillsList);
+                returnMap.put("dynamicproperties", dynamicpropertiesList);
+                returnMap.put("authentication", authenticationList);
+                returnMap.put("proxy", proxyList);
+            }
+        }
+        return returnMap;
+    }
+
+    private ResourceRecordSet getResourceRecordSetByServiceId(String id, List<ResourceRecordSet> resourceRecordSets) {
+        for (ResourceRecordSet resourceRecordSet : resourceRecordSets) {
+            System.out.println(resourceRecordSet.getName());
+            if (resourceRecordSet.getName().split("-")[1].equalsIgnoreCase(id)) {
+
+                return resourceRecordSet;
+            }
+        }
+        return null;
+    }
 
 }
